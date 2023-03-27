adce42c32e2695eb8648ca86d14e7ee940466296
Coderlancce <coderlancce@gmail.com>
2023-03-27 16:48:06 -0500

    comments added

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" review if some file is here """
+''' review if some file is here '''
+
 
 import sys
 import os
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    exit (1)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit (1)
 
-if not os.path.exists(sys.argv[1]):
-    sys.stderr.write('Missing ' + sys.argv[1] + '\n')
-    exit(1)
+    if not os.path.exists(sys.argv[1]):
+        sys.stderr.write('Missing ' + sys.argv[1] + '\n')
+        exit(1)
