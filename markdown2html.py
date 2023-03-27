d06c861594a93af227d1a3f81030f8ca332fdf50
Coderlancce <coderlancce@gmail.com>
2023-03-27 15:07:18 -0500

    [OK] check file present

---
+++ markdown2html.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print('Usage: ./markdown2html.py README.md README.html')
+    exit (1)
+
+if not os.path.exists(sys.argv[1]):
+    sys.stderr.write("Missing " + sys.argv[1] + "\n")
+    exit(1)
