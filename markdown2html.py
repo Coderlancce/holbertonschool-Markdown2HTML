2497ef760fb80ef3e981a4d87737638f8032e30b
Coderlancce <coderlancce@gmail.com>
2023-03-27 16:35:18 -0500

    OK delete bad identation

--- markdown2html.py
+++ markdown2html.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html\n")
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
     exit (1)
 
 if not os.path.exists(sys.argv[1]):
